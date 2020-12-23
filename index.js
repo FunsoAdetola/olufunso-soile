a26e2b5503cb63690fd5f07eb767d4f7bb02133c
Funsoadetola <{funsoadetola}@gmail.com>
2020-12-23 11:21:48 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const hamburger = document.querySelector(".hamburger");
+const navList = document.querySelector(".nav-list");
+const navItems = document.querySelectorAll(".nav-item");
+const menuLine = document.querySelectorAll(".line");
+const navLinks = document.querySelectorAll(".nav-link");
+
+
+function toggleMenu(){
+    navList.classList.toggle("open");
+    navItems.forEach((item)=>{
+        item.classList.toggle("fade");
+    })
+    menuLine.forEach((line)=>{
+        line.classList.toggle("show");
+    })
+}
+hamburger.addEventListener("click", ()=>{
+   toggleMenu();
+});
+
+navLinks.forEach((link)=>{
+    link.addEventListener("click", ()=>{
+     toggleMenu();
+})
+});
