2679bef163998894ab5a81059dacb4fb15ef72ce
Funsoadetola <{funsoadetola}@gmail.com>
2020-12-24 17:42:33 +0100

    new commit

--- index.js
+++ index.js
@@ -5,27 +5,25 @@ const menuLine = document.querySelectorAll(".line");
 const navLinks = document.querySelectorAll(".nav-link");
 const year = document.querySelector(".year");
 
-
-function toggleMenu(){
-    navList.classList.toggle("open");
-    navItems.forEach((item)=>{
-        item.classList.toggle("fade");
-    })
-    menuLine.forEach((line)=>{
-        line.classList.toggle("show");
-    })
+function toggleMenu() {
+  navList.classList.toggle("open");
+  navItems.forEach((item) => {
+    item.classList.toggle("fade");
+  });
+  menuLine.forEach((line) => {
+    line.classList.toggle("show");
+  });
 }
-hamburger.addEventListener("click", ()=>{
-   toggleMenu();
+hamburger.addEventListener("click", () => {
+  toggleMenu();
 });
 
-navLinks.forEach((link)=>{
-    link.addEventListener("click", ()=>{
-     toggleMenu();
-})
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    toggleMenu();
+  });
 });
 
-document.addEventListener("DOMContentLoaded", () =>{
-    year.innerText = new Date().getFullYear();
-})
-    
+document.addEventListener("DOMContentLoaded", () => {
+  year.innerText = new Date().getFullYear();
+});
